7d1f8989adf61f1e165f9ba5b6ffbdc4ac5e1b16
Himanshu <16himanshu16yadav@gmail.com>
2023-09-15 10:15:38 +0530

    m/

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const colors = require("colors");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect("mongodb+srv://himanshu16:himanshu@cluster0.vfz23.mongodb.net/sih?retryWrites=true&w=majority", {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     
