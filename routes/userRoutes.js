b5e86cf34eff3b2017b7182358a7fa88847d18a7
Himanshu <16himanshu16yadav@gmail.com>
2023-09-16 11:40:04 +0530

    sdf'

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,13 +5,14 @@ const {
   requestUser,
   getActiveRequestUser,
   getHistoryRequestUser,
+  getUser,
 
 } = require("../controllers/userControllers");
 const { protect } = require("../middleware/userAuthMiddleware");
 
 const router = express.Router();
 
-router.route("/get").get(registerUser);
+router.route("/get").get(protect,getUser);
 router.route("/history").get(protect, getHistoryRequestUser);
 router.route("/active").get(protect, getActiveRequestUser);
 router.route("/help").post(protect, requestUser);
