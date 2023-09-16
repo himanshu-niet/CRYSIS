af47a2ef3edeb6cb03425cc64e53b6dd47225ab6
Himanshu <16himanshu16yadav@gmail.com>
2023-09-16 11:52:12 +0530

    sdf'

--- middleware/userAuthMiddleware.js
+++ middleware/userAuthMiddleware.js
@@ -10,11 +10,10 @@ const protect = asyncHandler(async (req, res, next) => {
   let token;
 
   if (
-    req.headers.authorization &&
-    req.headers.authorization.startsWith("Bearer")
+    req.headers.authorization
   ) {
     try {
-      token = req.headers.authorization.split(" ")[1];
+      token = req.headers.authorization;
 
       //decodes token id
       const decoded = jwt.verify(token, JWT_SECRET);
